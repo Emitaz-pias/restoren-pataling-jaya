6f1b55af5a7d969ccd059c35f484efee4ee5f49f
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-10-12 16:05:50 +0600

    slider fixed

--- src/Components/Menu/Slider/Slider.js
+++ src/Components/Menu/Slider/Slider.js
@@ -4,178 +4,71 @@ import slide1 from "../../../images/pic1.jpg";
 import slide2 from "../../../images/pic2.jpg";
 import slide3 from "../../../images/pic3.jpg";
 import slider1 from "../../../images/slider4.jpg";
+import { Carousel } from "react-bootstrap";
 
 const OurSlider = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
-    <div>
+    <div style={{ marginLeft: "3%", marginRight: "3%" }} className="mt-3">
       {/* <h4 className="hotDealTitile">Promotions</h4> */}
-      <div className="">
-        <div className="sliderSection">
-          <div
-            id="carouselExampleIndicators"
-            class="carousel slide"
-            data-ride="carousel"
-          >
-            <ol class="carousel-indicators bg-danger">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="0"
-                class="active"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="1"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="2"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="3"
-              ></li>
-            </ol>
-            <div
-              style={{
-                marginLeft: "2.7%",
-                marginRight: "2%",
-                width: "93%",
-              }}
-              class="carousel-inner"
-            >
-              <div class="carousel-item active">
-                <div
-                  className="d-block"
-                  style={{ backgroundColor: "white", height: "20rem" }}
-                >
-                  <img
-                    style={{ borderRadius: "10px" }}
-                    className="sliderSectionImg w-100"
-                    src={slider1}
-                    alt="First slide"
-                  />
-                  {/* <div
-                    className="d-flex justify-content-between"
-                    style={{
-                      color: "black",
-                      marginTop: "0.625rem",
-                      lineHeight: "19.2px",
-                    }}
-                  >
-                    <p>Nasi Kukus Ayam Berempah</p>
-                    <small className="text-secondary">RM5.00</small>
-                  </div> */}
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div
-                  className="d-block"
-                  style={{
-                    backgroundColor: "white",
-                    height: "20rem",
-                  }}
-                >
-                  <img
-                    style={{ borderRadius: "10px" }}
-                    className="sliderSectionImg w-100"
-                    src={slider1}
-                    alt="First slide"
-                  />
-                  {/* <div
-                    className="d-flex  justify-content-between"
-                    style={{ color: "black", marginTop: "0.625rem" }}
-                  >
-                    <p style={{ lineHeight: "19.2px", fontSize: "15px" }}>
-                      Mee Goreng Mamak
-                    </p>
-                    <small className="text-secondary">RM6.00</small>
-                  </div> */}
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div
-                  className="d-block"
-                  style={{ backgroundColor: "white", height: "20rem" }}
-                >
-                  <img
-                    style={{ borderRadius: "10px" }}
-                    className="sliderSectionImg w-100"
-                    src={slider1}
-                    alt="First slide"
-                  />
-                  {/* <div
-                    className="d-flex  justify-content-between "
-                    style={{ color: "black", marginTop: "0.625rem" }}
-                  >
-                    <p style={{ lineHeight: "19.2px", fontSize: "15px" }}>
-                      Nasi Kukus Ayam Goreng Kunyit
-                    </p>
-                    <small className="text-secondary">RM6.00</small>
-                  </div> */}
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div
-                  className="d-block"
-                  style={{
-                    backgroundColor: "white",
-                    height: "20rem",
-                  }}
-                >
-                  <img
-                    style={{ borderRadius: "10px" }}
-                    className="sliderSectionImg w-100"
-                    src={slider1}
-                    alt="First slide"
-                  />
-                  {/* <div
-                    className="d-flex  justify-content-between"
-                    style={{ color: "black", marginTop: "0.625rem" }}
-                  >
-                    <p style={{ lineHeight: "19.2px", fontSize: "15px" }}>
-                      Mee Goreng Mamak
-                    </p>
-                    <small className="text-secondary">RM6.00</small>
-                  </div> */}
-                </div>
-              </div>
-            </div>
-            <a
-              class="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-              style={{ position: "absolute", top: "-8.2rem" }}
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-              style={{ position: "absolute", top: "-8.2rem" }}
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Next</span>
-            </a>
+      <Carousel interval={5000}>
+        <Carousel.Item
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <img
+            className="d-block w-100 sliderSectionImg"
+            src={slider1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <img
+            className="d-block w-100  sliderSectionImg"
+            src={slider1}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <img
+            className="d-block w-100  sliderSectionImg"
+            src={slider1}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <div>
+            <img
+              className="d-block w-100 sliderSectionImg"
+              src={slider1}
+              alt="Third slide"
+            />
           </div>
-        </div>
-      </div>
+        </Carousel.Item>
+        <Carousel.Item
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <img
+            className="d-block w-100 sliderSectionImg"
+            src={slider1}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
