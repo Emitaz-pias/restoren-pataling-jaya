1f7933ce4802d49f49f751ff08af449618da35c2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-10-17 03:04:24 +0600

    new design

--- src/Components/Menu/Header/Header.js
+++ src/Components/Menu/Header/Header.js
@@ -1,4 +1,8 @@
-import { faChevronLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faSearch,
+  faSignInAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -44,61 +48,78 @@ const Header = ({
   return (
     <div className={`${showHeader ? "fixedHeader" : "hideHeader"} bg-white`}>
       <div
-        style={{ height: "4rem" }}
+        style={{ height: "2rem", padding: "0.5rem 0 0.4rem 0" }}
         className="d-flex align-items-center  w-100"
       >
         {openSearch ? (
-          <div className="d-flex justify-content-around align-items-center w-100">
-            <FontAwesomeIcon
-              style={{ fontSize: "1.5rem", cursor: "pointer" }}
-              onClick={handleToggleSearch}
-              icon={faChevronLeft}
-            />
-            <form className="mb-2" onSubmit={handleSubmit(onSubmit)}>
-              <input
-                className="form-control-md "
-                type="text"
-                placeholder="Search"
+          <div className="w-100">
+            <div className="d-flex justify-content-around align-items-center">
+              <FontAwesomeIcon
                 style={{
-                  outline: "none",
-                  border: "none",
-                  borderBottom: "1px solid black",
+                  fontSize: "1.5rem",
+                  cursor: "pointer",
+                  marginLeft: "-3%",
                 }}
-                {...register("search", { required: true, maxLength: 80 })}
+                onClick={handleToggleSearch}
+                icon={faChevronLeft}
               />
-            </form>
-            <FontAwesomeIcon
-              style={{ fontSize: "1.5rem", cursor: "pointer" }}
-              className="me-3"
-              onClick={handleSubmit(onSubmit)}
-              icon={faSearch}
-            />
+              <form className="mb-2" onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  className="form-control-md "
+                  type="text"
+                  placeholder="Search"
+                  style={{
+                    outline: "none",
+                    border: "none",
+                    borderBottom: "1px solid black",
+                  }}
+                  {...register("search", { required: true, maxLength: 80 })}
+                />
+              </form>
+              <FontAwesomeIcon
+                style={{
+                  fontSize: "1.5rem",
+                  cursor: "pointer",
+                  position: "relative",
+                  left: "7%",
+                }}
+                className="me-3"
+                onClick={handleSubmit(onSubmit)}
+                icon={faSearch}
+              />
+            </div>
           </div>
         ) : (
-          <div
-            style={{ height: "4rem" }}
-            className=" d-flex align-items-center justify-content-around w-100"
-          >
+          <div className=" d-flex align-items-center justify-content-around w-100">
             {/* style={{ position: "relative", left: "40%" }} */}
             <FontAwesomeIcon
-              style={{ fontSize: "1.5rem", cursor: "pointer" }}
+              style={{
+                fontSize: "1.5rem",
+                cursor: "pointer",
+                marginLeft: "-12%",
+              }}
               onClick={handleToggleSearch}
               icon={faSearch}
             />
-            <p style={{ fontSize: "1.5rem", marginTop: "0.6rem" }}>SR Bistro</p>
-            <button
+            <h2
+              className="text-secondary"
               style={{
-                height: "1.8rem",
-                width: "5rem",
-                borderRadius: "7px",
-                border: "none",
-                color: "white",
-                paddingBottom: "0.5rem",
+                fontSize: "19px",
+                lineHeight: "23px",
+                marginTop: "0.6rem",
+                fontFamily: "Cormorant Garamond",
               }}
-              className="bg-dark"
             >
-              LogIn
-            </button>
+              SR Bistro
+            </h2>
+            <FontAwesomeIcon
+              style={{
+                fontSize: "1.5rem",
+                cursor: "pointer",
+                marginRight: "-11%",
+              }}
+              icon={faSignInAlt}
+            />
           </div>
         )}
       </div>
